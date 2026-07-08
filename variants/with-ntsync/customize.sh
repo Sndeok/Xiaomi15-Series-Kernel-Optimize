b744bb0ce02c96ba0167300587a5efb66090a6ff
SndeOK <Sndeok@users.noreply.github.com>
2026-07-08 12:06:24 +0800

    Release v6.8

--- variants/with-ntsync/customize.sh
+++ variants/with-ntsync/customize.sh
@@ -2,8 +2,8 @@
 ui_print "****************************************"
 ui_print " Xiaomi15-Series-Kernel-Optimize"
 ui_print " Performance / Memory / IO / Log / NTSync"
+ui_print " Version: v6.8"
 ui_print " Author: 酷安@Amktiao, GitHub@SndeOK"
-ui_print " Version: v6.6"
 ui_print "****************************************"
 
 print_done() {
